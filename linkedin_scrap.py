eb30f3250269123835cb2d9ba71a0fe705f05f7a
henrique <henriquealmeida1837@gmail.com>
2020-03-17 23:44:20 -0300

    fixed selenium memory issue, login function

--- linkedin_scrap.py
+++ linkedin_scrap.py
@@ -13,8 +13,27 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import time
 
+def login():
+    driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin")
+    input_email = driver.find_element_by_id("username")
+    input_email.click()
+    input_email.send_keys(email)
+    
+    input_pass = driver.find_element_by_id("password")
+    input_pass.click()
+    input_pass.send_keys(password)
+    
+    div_login = driver.find_element_by_class_name("login__form_action_container")
+    button_login = div_login.find_element_by_tag_name("button")
+    button_login.click()
+
 SELENIUM_PATH = "/home/henrique/Documents/selenium/geckodriver"
 CSV_FILENAME = "/home/henrique/Documents/scraps/linkedin_scrap/sample.csv"
+CSV_FILENAME_COMPLETE = "/home/henrique/Documents/scraps/linkedin_scrap/complete.csv"
+
+email = "iAmTheScrap@GetYourPublicData.com"
+password = "Some times we need to do that by force"
+
 
 url_base = "https://www.linkedin.com"
 url = "https://www.linkedin.com/school/univeritas/people/"
@@ -27,8 +46,11 @@ profile.set_preference("dom.webnotifications.enabled", False)
 
 
 driver = webdriver.Firefox(executable_path=SELENIUM_PATH,firefox_profile=profile)
-driver.get(url)
 
+
+login()
+
+driver.get(url)
 
 ## Funcao de login aqui ##
 
@@ -66,6 +88,8 @@ for i in range(len(a_links)):
     },
     ignore_index=True)
 
+## Saving Sample ##
+
 df.to_csv(path_or_buf=CSV_FILENAME)
 
 
@@ -74,17 +98,35 @@ df.to_csv(path_or_buf=CSV_FILENAME)
 
 driver = webdriver.Firefox(executable_path=SELENIUM_PATH,firefox_profile=profile)
 
+
+link = []
+name = []
 ocupation = []
 localization = []
 totalConnections = []
 about = []
 experience = []
 education = []
+
+df_final = pd.DataFrame({
+            "link": link ,
+            "name": name,
+            "ocupation": ocupation,
+            "localization": localization,
+            "totalConnections": totalConnections,
+            "about": about,
+            "experience": experience,
+            "education": education
+            })
+
+
 for i in range(len(df.link)):
+    link.append(df.link[i])
+    name.append(df.name[i])
     url_user = df.link[i]
     driver.get(url_user)
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-    time.sleep(0.8)
+    time.sleep(0.5)
     source = driver.page_source
     soup = BeautifulSoup(source,"lxml")
     user_resume = soup.find(attrs={"class":"pv-top-card"})
@@ -112,5 +154,27 @@ for i in range(len(df.link)):
         education.append(soup.find(attrs={"id":"education-section"}).text)
     except:
         education.append(None)
+    if(i%10 == 0 and i > 0):
+        print("Cleaning selenium memory")
+        df_final = df_final.append({
+            "link": link ,
+            "name": name,
+            "ocupation": ocupation,
+            "localization": localization,
+            "totalConnections": totalConnections,
+            "about": about,
+            "experience": experience,
+            "education": education
+        },ignore_index=True)
+        ocupation = []
+        localization = []
+        totalConnections = []
+        about = []
+        experience = []
+        education = []
+        driver.close()
+        time.sleep(1)
+        driver = webdriver.Firefox(executable_path=SELENIUM_PATH,firefox_profile=profile)
+        login()
     
     print("Getting data from "+str(i+1)+" of "+str(len(df.link)))
